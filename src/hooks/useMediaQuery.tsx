a9866261e9c3f07f17a11a49f3d453c9dd6b0c69
Cindy Tao <cindytao.xy@outlook.com>
2024-07-30 16:42:52 -0400

    tailwind setup

---
+++ src/hooks/useMediaQuery.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+// determine what breakpoint our current media meet
+const useMediaQuery = (query: string): boolean => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    window.addEventListener("resize", listener);
+    return () => window.removeEventListener("resize", listener);
+  }, [matches, query]);
+
+  return matches;
+};
+
+export default useMediaQuery;
